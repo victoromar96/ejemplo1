a289692c322c7537adc5d9ed6190e79e33091b99
luis.sanmartin@epn.edu.ec <luis.sanmartin@epn.edu.ec>
2018-04-26 10:36:50 -0500

    Clase

---
+++ Sanmartin_0466+.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 26 10:34:07 2018
+
+@author: Fernando Sanmartin
+"""
+print ("Fernando Sanmartin")
+
+ci = 1718409988
+suma = 1+7+1+8+4+0+9+9+8+8
+print (suma)
